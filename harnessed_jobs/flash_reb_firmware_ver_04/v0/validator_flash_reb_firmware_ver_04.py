f66113a9437089211a795a8458963dacfeb3f9e7
Jim Chiang <jchiang@slac.stanford.edu>
2016-12-09 18:57:30 -0800

    no-op validator script for firmware flash

--- harnessed_jobs/flash_reb_firmware_ver_04/v0/validator_flash_reb_firmware_ver_04.py
+++ harnessed_jobs/flash_reb_firmware_ver_04/v0/validator_flash_reb_firmware_ver_04.py
@@ -1,25 +1,2 @@
 #!/usr/bin/env python
-import os
-import lcatr.schema
-#import lcatr.harness.et_wrapper
-import eTraveler.clientAPI.connection
-import rebUtils
-
-ccs_subsystem = 'ccs-reb4'
-
-manufacturerSN = \
-    rebUtils.get_serial_number_from_board(ccs_subsystem=ccs_subsystem)
-try:
-#    lcatr.harness.et_wrapper.setManufacturerId(manufacturerId=manufacturerSN)
-    rebUtils.setManufacturerId(manufacturerId=manufacturerSN)
-except Exception as eobj:
-    print eobj
-
-rebUtils.check_serial_number(ccs_subsystem=ccs_subsystem)
-print "manufacturerSN set to", manufacturerSN
-
-results = [lcatr.schema.valid(lcatr.schema.get('sernum_persist_ver_01'),
-                              manufacturerSN=manufacturerSN)]
-
-lcatr.schema.write_file(results)
-lcatr.schema.validate_file()
+# No results or data products, so all the work is done in the producer script.
