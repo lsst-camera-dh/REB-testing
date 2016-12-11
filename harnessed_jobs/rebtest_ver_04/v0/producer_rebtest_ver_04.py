c9fd8b1766f1ddeee4662652aebf15cda0dee5c6
Jim Chiang <jchiang@slac.stanford.edu>
2016-12-10 16:57:45 -0800

    check serial number against value in eT tables

--- harnessed_jobs/rebtest_ver_04/v0/producer_rebtest_ver_04.py
+++ harnessed_jobs/rebtest_ver_04/v0/producer_rebtest_ver_04.py
@@ -2,6 +2,10 @@
 from __future__ import print_function
 import os
 import subprocess
+import rebUtils
+
+ccs_subsystem = 'ccs-reb5-0'
+rebUtils.check_serial_number(ccs_subsystem)
 
 reb_id = os.environ["LCATR_UNIT_ID"].split('-')[1]
 
