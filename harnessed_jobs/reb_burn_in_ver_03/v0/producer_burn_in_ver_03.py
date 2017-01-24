ce56cc9a1ff53b0d4c1a822763be4b454b4548d6
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-23 21:35:47 -0800

    commands to drive burn-in script

--- harnessed_jobs/reb_burn_in_ver_03/v0/producer_burn_in_ver_03.py
+++ harnessed_jobs/reb_burn_in_ver_03/v0/producer_burn_in_ver_03.py
@@ -9,9 +9,14 @@ import rebUtils
 
 subsystems = ['ccs-reb5-%i' % i for i in range(3)]
 ccs_subsystem = rebUtils.get_ccs_subsystem(subsystems)
+results_file = 'burn_in_script_output.txt'
+with open(results_file, 'w') as output:
+    output.write(ccs_subsystem + '\n')
 
 burn_in_script = '/lsst/ccs/REBtest/REB5Test.py'
-command = "%(burn_in_script)s %(ccs_subsystem)s > %(outfile)s" % locals()
+cwd = os.path.abspath('.')
+
+command = "%(burn_in_script)s -n -v -C %(ccs_subsystem)s $(cwd)" % locals()
 print(command)
 
 subprocess.check_call(command, shell=True, executable='/bin/bash')
