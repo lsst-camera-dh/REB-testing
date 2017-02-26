8f46acff54904c16916e91a5e589f96421781818
Jim Chiang <jchiang@slac.stanford.edu>
2017-02-26 08:53:18 -0800

    check if LCATR_UNIT_ID is set

--- bin/make_trending_plots.py
+++ bin/make_trending_plots.py
@@ -2,6 +2,7 @@
 """
 Application to make CCS trending plots given a config file.
 """
+import os
 import argparse
 import rebUtils
 
@@ -17,6 +18,9 @@ parser.add_argument("--end_datetime", type=str, default=None,
 parser.add_argument("--nbins", type=int, default=100,
                     help="Number of time bins.  Default: 100")
 args = parser.parse_args()
+
+if not os.environ.has_key('LCATR_UNIT_ID'):
+    os.environ['LCATR_UNIT_ID'] = 'UNSET_LCATR_UNIT_ID'
 
 rebUtils.make_ccs_trending_plots(args.ccs_subsystem,
                                  dt=args.dt,
